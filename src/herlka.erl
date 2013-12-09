60079056ecb3213ceeeee8addb505bb512336d37
Vincent Chinedu Okonkwo <chinedu@saya.im>
2013-12-09 20:33:47 +0000

    minor cleanup

--- src/herlka.erl
+++ src/herlka.erl
@@ -9,21 +9,6 @@ start_link() -> ok.
 %% @spec start() -> ok
 %% @doc Start the shawarma server.
 start() ->
-  application:set_env(herlka, loggers, [
-      {rlog, [{transport,udp},
-        {host,"localhost"},
-        {port,5565},
-        {hmac_key, <<"xdd908lfcgikauexdi8elogusridaxoalf">>},
-        {hmac_signer, <<"ops">>},
-        {hmac_key_ver, 1}]},
-
-      {rlog2, [{transport,udp},
-        {host,"localhost"},
-        {port,5565},
-        {hmac_key, <<"haeoufyaiofeugdsnzaogpi.ua,dp.804u">>},
-        {hmac_signer, <<"dev">>},
-        {hmac_key_ver, 1}]}
-    ]),
   ensure_started(?MODULE).
 
 
@@ -39,11 +24,9 @@ stop() ->
 ensure_started(App) ->
   case application:start(App) of
     ok -> ok;
-    { error, { already_started, App } } -> ok;
+    { error, {already_started, App} } -> ok;
     {error, {not_started, Dep}} -> 
       ok = ensure_started(Dep),
       ensure_started(App);
-    _Error ->
-      io:format("startup error: ~p~n", [_Error]),
-      erlang:halt()
+    _E -> _E
   end.
