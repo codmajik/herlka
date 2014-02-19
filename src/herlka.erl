8992cdd705e791bf96d5121a5f66348c67ad9b93
Vincent Chinedu Okonkwo <chinedu@gmail.com>
2014-02-19 23:09:11 +0000

    added high level functions herlka:log()/s

--- src/herlka.erl
+++ src/herlka.erl
@@ -2,6 +2,12 @@
 
 -export([start/0, start_link/0, stop/0]).
 
+-export([log/2, log/4, log/5, log/6, log/7, log/8]).
+
+-include("include/herlka.hrl").
+
+
+
 %% @spec start_link() -> {ok,Pid::pid()}
 %% @doc Starts the shawarma app for inclusion in a supervisor tree
 start_link() -> ok.
@@ -16,6 +22,56 @@ start() ->
 %% @doc Stop the shawarma server.
 stop() ->
   application:stop(?MODULE).
+
+-spec log(atom() | pid(), string(), integer(), string()) -> ok.
+log(Logger, Type, Severity, LogMsg) ->
+  log(Logger, Type, Severity, LogMsg, []).
+
+-spec log(atom() | pid(), string(), integer(), string(), 
+  list({string(),string()})) -> ok.
+log(Logger, Type, Severity, LogMsg, Metadata) ->
+  log(Logger, Type, Severity, LogMsg, Metadata, erlang:now()).
+
+-spec log(atom() | pid(), string(), integer(), string(), 
+  list({string(),string()}), calender:datetime() | erlang:timestamp()) -> ok.
+log(Logger, Type, Severity, LogMsg, Metadata, Timestamp) ->
+  log(Logger, Type, Severity, LogMsg, Metadata, Timestamp,  self()).
+
+-spec log(atom() | pid(), string(), integer(), string(), 
+  list({string(),string()}), calender:datetime() | erlang:timestamp(), pid()) -> ok.
+log(Logger, Type, Severity, LogMsg, Metadata, Timestamp,  SenderPid) ->
+  {ok,DefApp} = application:get_application(),
+  log(Logger, Type, Severity, LogMsg, Metadata, Timestamp,  SenderPid, DefApp).
+
+-spec log(atom() | pid(), string(), integer(), string(), 
+  list({string(),string()}), calender:datetime() | erlang:timestamp(), 
+    pid(), atom())  -> ok.
+log(Logger, Type, Severity, LogMsg, Metadata, Timestamp,  SenderPid,  App) ->
+  log(Logger, #herlka_msg {
+        type = Type,
+        timestamp = herlka_utils:extract_epoch(Timestamp),
+        pid = herlka_utils:pid_to_int(SenderPid),
+        severity = Severity,
+        payload = LogMsg,
+        logger = App,
+        fields = Metadata
+      }).
+
+-spec log(atom() | pid(), #herlka_msg{}) -> ok.
+log(Logger, #herlka_msg {  type = T,  timestamp=Ts,  pid=P,  
+                severity=S,  payload=M, logger=A,  fields=[]  } = HM )  
+  when  (is_atom(Logger) orelse is_pid(Logger)), is_binary(T), is_integer(Ts), 
+    is_pid(P), is_integer(S), is_binary(M), is_atom(A)  
+    ->  herlka_logger:send(Logger, HM);
+
+
+log(Logger, #herlka_msg {  type = T,  timestamp=Ts,  pid=P,  
+                severity=S,  payload=M, logger=A, fields=[{_,_}|_] } = HM )  
+  when  (is_atom(Logger) orelse is_pid(Logger)), is_binary(T), is_integer(Ts), 
+    is_pid(P), is_integer(S), is_binary(M), is_atom(A)   ->  
+    herlka_logger:send(Logger, HM).
+
+
 
 %% ===================================================================
 %% Internal Functions
